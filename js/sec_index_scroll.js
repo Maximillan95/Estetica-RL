9c8231a0cb3f67084acd7a4c9d3ae2cd5461da32
Maximiliano Millan <maxi_millan23@hotmail.com>
2022-10-21 08:54:44 -0300

    Agregué a cada CSS el codigo para adaptar a dispositivos, se hizo el responsive al index.html

--- js/sec_index_scroll.js
+++ js/sec_index_scroll.js
@@ -2,7 +2,7 @@ let infoServIndex = document.querySelector('.animado');
 
 function mostrarInfomacionServicio(){
     let scrollTop = document.documentElement.scrollTop; //scrollTop calcula a qué altura de la pag está
-        if(scrollTop >= 530){
+        if(scrollTop >= 500){
             infoServIndex.style.opacity = 1;
             infoServIndex.classList.add('mostrarArriba');
         }
